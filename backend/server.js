182718760fb9073b36a924e1f91c59604f7ced82
Devin W Bowler <83326036+devinbowler@users.noreply.github.com>
2023-11-09 15:10:54 -0500

    Update server.js
    Changed hosting API routes, and integrated CORS.

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,37 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
-const Task = require('./models/taskModel'); 
+const cors = require('cors');
+const Task = require('./models/taskModel');
 
 const scheduleRoutes = require('./routes/schedule')
 const taskRoutes = require('./routes/task')
 const userRoutes = require('./routes/user')
 
-
 // Express app
 const app = express();
 
-// Miiddleware
+// Allowed origins
+const allowedOrigins = [
+  'https://your-frontend-domain.netlify.app',
+  'http://localhost:3000', // Add your local development origin here
+  // ... any other domains you want to whitelist
+];
+
+// CORS configuration
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
+}));
+
+// Middleware
 app.use(express.json())
 
 app.use((req, res, next) => {
@@ -24,13 +44,13 @@ app.use('/api/schedule', scheduleRoutes)
 app.use('/api/tasks', taskRoutes)
 app.use('/api/user', userRoutes)
 
-// connect to db
+// connect to db and start server
 mongoose.connect(process.env.MONG_URI)
     .then(() => {
         // listen for requests
         app.listen(process.env.PORT, () => {
-            console.log('Connected to DB: Port', process.env.PORT)
-    })
+            console.log('Connected to DB & Listening on port', process.env.PORT)
+        })
     })
     .catch((error) => {
         console.log(error)
