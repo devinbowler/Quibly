cd081042c70b81a2fd30b7eb107dfd2d2a310a2f
Devin W Bowler <83326036+devinbowler@users.noreply.github.com>
2023-11-09 23:30:32 -0500

    Update server.js
    Cors Update.

--- backend/server.js
+++ backend/server.js
@@ -1,57 +1,57 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const Task = require('./models/taskModel');
 
-const scheduleRoutes = require('./routes/schedule')
-const taskRoutes = require('./routes/task')
-const userRoutes = require('./routes/user')
+const scheduleRoutes = require('./routes/schedule');
+const taskRoutes = require('./routes/task');
+const userRoutes = require('./routes/user');
 
 // Express app
 const app = express();
 
-// Allowed origins
+// Allowed origins for CORS
 const allowedOrigins = [
-  'https://your-frontend-domain.netlify.app',
-  'http://localhost:3000', // Add your local development origin here
-  // ... any other domains you want to whitelist
+  'https://quantumix.net', // Your Netlify domain
+  'http://localhost:3000', // Your local development domain
+  // You can add any other domains you want to whitelist here
 ];
 
 // CORS configuration
 app.use(cors({
   origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
+    if (!origin) return callback(null, true); // Allow requests with no origin (like mobile apps, curl, etc.)
     if (allowedOrigins.indexOf(origin) === -1) {
-      var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
       return callback(new Error(msg), false);
     }
     return callback(null, true);
   },
-  credentials: true,
+  credentials: true, // Allow sending credentials like cookies and HTTP authentication
 }));
 
 // Middleware
-app.use(express.json())
+app.use(express.json());
 
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
+    console.log(req.path, req.method);
+    next();
+});
 
 // Routes
-app.use('/api/schedule', scheduleRoutes)
-app.use('/api/tasks', taskRoutes)
-app.use('/api/user', userRoutes)
+app.use('/api/schedule', scheduleRoutes);
+app.use('/api/tasks', taskRoutes);
+app.use('/api/user', userRoutes);
 
-// connect to db and start server
+// Connect to DB and start server
 mongoose.connect(process.env.MONG_URI)
     .then(() => {
-        // listen for requests
+        // Listen for requests
         app.listen(process.env.PORT, () => {
-            console.log('Connected to DB & Listening on port', process.env.PORT)
-        })
+            console.log('Connected to DB & Listening on port', process.env.PORT);
+        });
     })
     .catch((error) => {
-        console.log(error)
-    })
+        console.log(error);
+    });
