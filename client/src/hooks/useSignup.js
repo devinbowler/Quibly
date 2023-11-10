09eb11f0ccfb650d7f4f44dfc8b43a64592d3572
Devin W Bowler <83326036+devinbowler@users.noreply.github.com>
2023-11-09 23:45:22 -0500

    Update useSignup.js
    Error Handleing.

--- client/src/hooks/useSignup.js
+++ client/src/hooks/useSignup.js
@@ -2,36 +2,38 @@ import { useState } from 'react'
 import { useAuthContext } from './useAuthContext'
 
 export const useSignup = () => {
-    const [error, setError] = useState(null)
-    const [isLoading, setIsLoading] = useState(null)
-    const { dispatch } = useAuthContext()
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(false); // Default should be false
+    const { dispatch } = useAuthContext();
 
     const signup = async (email, password) => {
         setIsLoading(true);
         setError(null);
-    
-        const response = await fetch('https://quantumix.onrender.com/api/user/signup', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({email, password})
-        });
-        const json = response.json()
 
-        if (!response.ok) {
-            setIsLoading(false)
-            setError(json.error)
+        try {
+            const response = await fetch('https://quantumix.onrender.com/api/user/signup', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({email, password})
+            });
+
+            const json = await response.json(); // Await the resolution of json()
+
+            if (!response.ok) {
+                setIsLoading(false);
+                setError(json.error); // Use the error from the response
+            }
+
+            if (response.ok) {
+                localStorage.setItem('user', JSON.stringify(json)); // Save user to local storage
+                dispatch({type: 'LOGIN', payload: json}); // Update auth context
+                setIsLoading(false);
+            }
+        } catch (err) {
+            setIsLoading(false);
+            setError('Failed to sign up'); // Generic error message for network errors
         }
+    };
 
-        if (response.ok) {
-            // save user to local storage
-            localStorage.setItem('user', JSON.stringify(json))
-
-            // update auth context
-            dispatch({type: 'LOGIN', payload: json})
-
-            setIsLoading(false)
-        }
-    }
-
-    return { signup, isLoading, error}
-}
+    return { signup, isLoading, error };
+};
