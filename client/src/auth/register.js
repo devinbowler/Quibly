a8b652a7ae715e47eae698d10b008278d25b910a
Devin W Bowler <83326036+devinbowler@users.noreply.github.com>
2023-11-10 05:15:45 -0500

    Update register.js

--- client/src/auth/register.js
+++ client/src/auth/register.js
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import './register.css';
 import { useSignup } from "../hooks/useSignup"
 import { useNavigate } from "react-router-dom";
+import { GoogleLogin } from 'react-google-login';
 
 const Register = () => {
   const [email, setEmail] = useState('')
@@ -12,10 +13,23 @@ const Register = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log('Submitting with Email:', email, 'Password:', password);
+    // console.log('Submitting with Email:', email, 'Password:', password);
     const success = await signup(email, password);
     if(success) navigate('/login'); // Navigate to login page after successful registration
   }
+
+  const responseGoogle = (response) => {
+    console.log(response);
+  
+    if (response.profileObj) {
+      const { email, googleId } = response.profileObj; // Extract email and Google ID
+      // Send this data to your backend for further processing
+    } else {
+      // Handle login failure
+      console.error('Google Login was unsuccessful');
+    }
+  };
+  
 
     return (
         <div className="register-container">
@@ -25,10 +39,14 @@ const Register = () => {
             <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
             <input type="password" placeholder="Confirm Password" />
             <button type="submit" disabled={isLoading}>Sign Up with Email</button>
-            <a href="#" className="login-with-google">
-              <img src={process.env.PUBLIC_URL + "/assets/google_logo.svg"} alt="Google Logo" />
-              Login with Google
-            </a>
+            <GoogleLogin
+              clientId={process.env.REACT_APP_CLIENT_ID}
+              buttonText="Login with Google"
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy={'single_host_origin'}
+              className="login-with-google"
+            />
             <p className="login-text">Already have an account? <a href="/login">Log In</a></p>
             {error && <div className='error'>{error}</div>}
           </form>
