fbe09d98c403b534dbad0ea3ea5666852023472f
Devin W Bowler <83326036+devinbowler@users.noreply.github.com>
2023-11-10 20:37:55 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,7 @@ app.use(express.json());
 
 app.use((req, res, next) => {
     // console.log(`Incoming request: ${req.method} ${req.path}`);
+    console.log(`Incoming request: ${req.method} ${req.path}, Body:`, req.body);
     next();
 });
 // Routes
