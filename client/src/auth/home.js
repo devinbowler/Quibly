72337cf83da10651abb9229517acc27a44a13832
Devin W Bowler <83326036+devinbowler@users.noreply.github.com>
2023-11-17 06:35:12 -0500

    Update home.js

--- client/src/auth/home.js
+++ client/src/auth/home.js
@@ -10,44 +10,33 @@ const AuthHome = () => {
 
   return (
     <div className="home-page" id="home">
+      {/* Navbar */}
       <nav className="navbar">
-        <img src={process.env.PUBLIC_URL + "/assets/logoTansp.png"} alt="Logo" className="navbar-logo desktop-only" />
+        <span className="quantumix-nav-title">Quantumix</span>
         <span className="hamburger-menu mobile-only" onClick={handleMenuClick}>&#9776;</span>
         <ul className={`nav-menu${showMenu ? " show" : ""}`}>
           <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
-          <li><a href="#contact">Contact</a></li>
-          <li className="right-float"><a href="./register" className="start-for-free">Start for Free</a></li>
+          <li className="right-float"><a href="./register" className="start-for-free">Get Started for Free</a></li>
         </ul>
       </nav>
+
+      {/* Main Content */}
       <div className="main-content">
-        <h1 className="quantumix-title">Quantumix</h1>
-        <p className="slogan">Finally unlock your  true potential.<br/>Conquer every challenge.<br/>Empower your journey with Quantumix.</p>
-        <li className="li-center">
-           <a href="./register" className="start-for-free-center">Start for Free</a>
-        </li>
-      </div>
-      <footer className="footer" id="contact">
-        <div className="social-icons">
-            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-            <img src={process.env.PUBLIC_URL + "/assets/instagram.png"} alt="Instagram" />
-            </a>
-            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-            <img src={process.env.PUBLIC_URL + "/assets/twitter.png"} alt="Twitter" />
-            </a>
-
-            <a href="/" rel="noopener noreferrer">
-            <img className="footer-logo" src={process.env.PUBLIC_URL + "/assets/logoTansp.png"} alt="Footer Logo" />
-            </a>
-
-            <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
-            <img src={process.env.PUBLIC_URL + "/assets/youtube.png"} alt="YouTube" />
-            </a>
-            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-            <img src={process.env.PUBLIC_URL + "/assets/facebook.png"} alt="Facebook" />
-            </a>
+        <div className="content-section">
+          <h1 className="main-title">
+            Begin to <br /> manage your <br /> life with
+          </h1>
+          <div className="dynamic-text">
+            <li><span className="type-animation">scheduling.</span></li>
+            <li><span className="type-animation">tasks.</span></li>
+            <li><span className="type-animation">planning.</span></li>
+          </div>
+          <p className="main-description">Quantumix is an all-in-one productivity app <br /> designed to streamline the management of <br />schedules, tasks, projects, and more.</p>
+          <a href="./register" className="start-for-free">Get Started for Free</a>
+          <a href="#" className="contact-us">Contact The Team</a>
         </div>
-        </footer>
+      </div>
     </div>
   );
 };
