4d711c96c886136641da8218a9ae2d4aff03e4ba
Devin W Bowler <83326036+devinbowler@users.noreply.github.com>
2023-11-20 22:00:47 -0500

    Update home.js

--- client/src/auth/home.js
+++ client/src/auth/home.js
@@ -1,18 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './home.css';
+import QuantumixLogo from '../images/Quantumix-A1.png'; // Adjust the path as necessary
+
 
 const AuthHome = () => {
   const [showMenu, setShowMenu] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   const handleMenuClick = () => {
     setShowMenu(!showMenu);
+  };
+
+  const renderDynamicText = () => {
+    if (windowWidth <= 768) {
+      // If the screen size is 768px or less, display "Quantumix"
+      return <span className="type-animation">Quantumix</span>;
+    } else {
+      // Otherwise, display the animated text
+      return (
+        <>
+          <li><span className="type-animation">scheduling.</span></li>
+          <li><span className="type-animation">tasks.</span></li>
+          <li><span className="type-animation">planning.</span></li>
+        </>
+      );
+    }
   };
 
   return (
     <div className="home-page" id="home">
       {/* Navbar */}
       <nav className="navbar">
-        <span className="quantumix-nav-title">Quantumix</span>
+      <span className="quantumix-nav-title">
+        <img src={QuantumixLogo} alt="Quantumix Logo" />
+      </span>
         <span className="hamburger-menu mobile-only" onClick={handleMenuClick}>&#9776;</span>
         <ul className={`nav-menu${showMenu ? " show" : ""}`}>
           <li><a href="#home">Home</a></li>
@@ -28,9 +58,7 @@ const AuthHome = () => {
             Begin to <br /> manage your <br /> life with
           </h1>
           <div className="dynamic-text">
-            <li><span className="type-animation">scheduling.</span></li>
-            <li><span className="type-animation">tasks.</span></li>
-            <li><span className="type-animation">planning.</span></li>
+            {renderDynamicText()}
           </div>
           <p className="main-description">Quantumix is an all-in-one productivity app <br /> designed to streamline the management of <br />schedules, tasks, projects, and more.</p>
           <a href="./register" className="start-for-free">Get Started for Free</a>
@@ -38,6 +66,8 @@ const AuthHome = () => {
         </div>
       </div>
     </div>
+
+    
   );
 };
 
