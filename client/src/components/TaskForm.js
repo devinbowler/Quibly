967f9f20f2d4ee6df5731892892fa38e5ace04e8
Devin W Bowler <83326036+devinbowler@users.noreply.github.com>
2023-11-20 22:02:14 -0500

    Update TaskForm.js

--- client/src/components/TaskForm.js
+++ client/src/components/TaskForm.js
@@ -8,19 +8,10 @@ const TaskForm = ({ onAddTask, closeForm, editTitle = "", editDueTime = "", edit
     const [title, setTitle] = useState(editTitle);
     const [dueTime, setDueTime] = useState(editDueTime || '');
     const [description, setDescription] = useState(editDescription);
-    const [taskType, setTaskType] = useState('Single');
-    const [projectFolder, setProjectFolder] = useState('');
     const { darkMode } = useTheme();
 
     const { user } = useAuthContext()
 
-    const handleTaskTypeChange = (e) => {
-        setTaskType(e.target.value);
-    };
-
-    const handleProjectFolderChange = (e) => {
-        setProjectFolder(e.target.value);
-    };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -47,7 +38,6 @@ const TaskForm = ({ onAddTask, closeForm, editTitle = "", editDueTime = "", edit
         console.log("Title:", title);
         console.log("Due Time:", dueTime);
         console.log("Description:", description);
-        console.log("Task Type:", taskType);
         console.log("User ID:", user ? user.token : "User not logged in");
 
 
@@ -56,48 +46,46 @@ const TaskForm = ({ onAddTask, closeForm, editTitle = "", editDueTime = "", edit
             title: title,
             desc: description,
             timer: `${dueTime}:00`,  // You may want to update this based on how you use it in the backend
-            dueDate: dueDate,  // Add this line
-            paused: false,  // Add this line
-            pauseStartTime: null,  // Add this line
+            dueDate: dueDate,  
+            paused: false,  
+            pauseStartTime: null, 
             secondsLeft: secondsLeft,
-            type: taskType,
-            user_id: user.token
+            user_id: user.token,
+            type: "YourTaskTypeHere"  // Add the type field here
         };
 
         console.log("Data being sent to the server:", newTask);
 
 
         try {
-            // Make an API call to create the new task using fetch API
             const apiUrl = `https://quantumix.onrender.com/api/tasks${isEditing ? `/${editTitle}` : ''}`;
             const method = isEditing ? 'PUT' : 'POST';
 
             const response = await fetch(apiUrl, {
                 method: method,
                 headers: {
-                    'Authorization': `Bearer ${user.token}`,  // Using user's token
+                    'Authorization': `Bearer ${user.token}`,
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify(newTask)
             });
 
-            let responseData = await response.json(); // Only calling response.json() once
+            const responseData = await response.json();
 
-            if (!response.ok) {
-                console.error('Response status:', response.status);
-                console.error('Response status text:', response.statusText);
-                console.error('Response body:', responseData);
+            if (response.ok) {
+                // If the task is successfully created, use the response data
+                // Assuming the API returns the created task, including its ID
+                onAddTask(responseData);
+                // Close the form after task is added
+                closeForm();
             } else {
-                onAddTask(title, dueTime, description);
+                console.error('Failed to create task:', responseData);
+                alert('Failed to create task. Please try again.');
             }
         } catch (err) {
             console.error('An error occurred while creating the task:', err);
+            alert('An error occurred. Please try again.');
         }
-
-        setTitle('');
-        setDueTime('');
-        setDescription('');
-        closeForm();
     };
 
     return (
@@ -132,25 +120,6 @@ const TaskForm = ({ onAddTask, closeForm, editTitle = "", editDueTime = "", edit
                             placeholder="Description"
                             onChange={(e) => setDescription(e.target.value)}
                         />
-                        <select
-                            className={`styled-select ${darkMode ? 'dark-styled-select' : ''}`}
-                            value={taskType}
-                            onChange={handleTaskTypeChange}
-                            required
-                        >
-                            <option value="" disabled hidden>Task Type</option>
-                            <option value="Single">Single</option>
-                            <option value="Routine">Routine</option>
-                            <option value="Project">Project</option>
-                        </select>
-                        {taskType === 'Project' && (
-                            <input
-                                type="text"
-                                value={projectFolder}
-                                onChange={handleProjectFolderChange}
-                                placeholder="Enter project folder name or select..."
-                            />
-                        )}
                         <button type="submit">{isEditing ? "Save" : "Create Task"}</button>
                     </form>
                 </div>
