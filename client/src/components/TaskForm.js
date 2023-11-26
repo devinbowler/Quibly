c983f6a1610764c36122a054f690a4b1ed61c33f
Devin W Bowler <83326036+devinbowler@users.noreply.github.com>
2023-11-26 18:55:55 -0500

    Update TaskForm.js

--- client/src/components/TaskForm.js
+++ client/src/components/TaskForm.js
@@ -4,128 +4,183 @@ import './taskForm.css';
 import { useAuthContext } from '../hooks/useAuthContext';
 import { useTheme } from '../ThemeContext';
 
-const TaskForm = ({ onAddTask, closeForm, editTitle = "", editDueTime = "", editDescription = "", isEditing = false }) => {
-    const [title, setTitle] = useState(editTitle);
-    const [dueTime, setDueTime] = useState(editDueTime || '');
-    const [description, setDescription] = useState(editDescription);
-    const { darkMode } = useTheme();
+const TaskForm = ({ task, onAddTask, onUpdateTask, onDeleteTask, closeForm, isEditing }) => {
+  const [title, setTitle] = useState(task?.title || '');
+  const [description, setDescription] = useState(task?.desc || '');
+  const [tags, setTags] = useState(Array.isArray(task?.tags) ? task.tags : []);
+  const [dueTime, setDueTime] = useState(task?.dueDate ? new Date(task.dueDate).toISOString().substring(0, 16) : '');
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const { user } = useAuthContext();
+  const { darkMode } = useTheme();
 
-    const { user } = useAuthContext()
+  const handleTagChange = (index, event) => {
+    const newTags = [...tags];
+    newTags[index][event.target.name] = event.target.value;
+    setTags(newTags);
+  };
 
+  const addTag = () => {
+    setTags([...tags, { name: '', color: '#000000' }]);
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const removeTag = index => {
+    const newTags = [...tags];
+    newTags.splice(index, 1);
+    setTags(newTags);
+  };
 
-        // Check if the user is logged in
-        if (!user) {
-            console.error('You must be logged in');
-            return;
-        }
+  const handleDelete = async () => {
+    if (!user || !task) return;
 
-        // Calculate the difference between the selected time and the current time in seconds
-        const selectedDate = new Date(dueTime);
-        const now = new Date();
-        const secondsLeft = Math.floor((selectedDate - now) / 1000);
-        const dueDate = selectedDate.toISOString();  // Convert the date to a string
+    console.log('deleting')
 
+    const response = await fetch(`https://quantumix.onrender.com/api/tasks/${task._id}`, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Bearer ${user.token}`
+      }
+    });
 
-        if (isNaN(secondsLeft) || secondsLeft <= 0) {
-            console.error('Invalid secondsLeft value');
-            alert('Please enter a valid future date and time.');
-            return;
-        }
+    if (response.ok) {
+      onDeleteTask(task._id); // Update parent state
+      closeForm();
+    }
+  };
 
-        console.log("Title:", title);
-        console.log("Due Time:", dueTime);
-        console.log("Description:", description);
-        console.log("User ID:", user ? user.token : "User not logged in");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!user) {
+      console.error('You must be logged in');
+      return;
+    }
 
+    const selectedDate = new Date(dueTime);
+    const now = new Date();
+    const secondsLeft = Math.floor((selectedDate - now) / 1000);
+    const dueDate = selectedDate.toISOString();
 
-        // Prepare the task object
-        const newTask = {
-            title: title,
-            desc: description,
-            timer: `${dueTime}:00`,  // You may want to update this based on how you use it in the backend
-            dueDate: dueDate,  
-            paused: false,  
-            pauseStartTime: null, 
-            secondsLeft: secondsLeft,
-            user_id: user.token,
-            type: "YourTaskTypeHere"  // Add the type field here
-        };
+    if (isNaN(secondsLeft) || secondsLeft <= 0) {
+      console.error('Invalid secondsLeft value');
+      alert('Please enter a valid future date and time.');
+      return;
+    }
 
-        console.log("Data being sent to the server:", newTask);
-
-
-        try {
-            const apiUrl = `https://quantumix.onrender.com/api/tasks${isEditing ? `/${editTitle}` : ''}`;
-            const method = isEditing ? 'PUT' : 'POST';
-
-            const response = await fetch(apiUrl, {
-                method: method,
-                headers: {
-                    'Authorization': `Bearer ${user.token}`,
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(newTask)
-            });
-
-            const responseData = await response.json();
-
-            if (response.ok) {
-                // If the task is successfully created, use the response data
-                // Assuming the API returns the created task, including its ID
-                onAddTask(responseData);
-                // Close the form after task is added
-                closeForm();
-            } else {
-                console.error('Failed to create task:', responseData);
-                alert('Failed to create task. Please try again.');
-            }
-        } catch (err) {
-            console.error('An error occurred while creating the task:', err);
-            alert('An error occurred. Please try again.');
-        }
+    const newTask = {
+      title,
+      desc: description,
+      timer: `${dueTime}:00`,
+      dueDate,
+      paused: task?.paused || false,
+      pauseStartTime: task?.pauseStartTime || null,
+      secondsLeft,
+      user_id: user.token,
+      tags
     };
 
-    return (
-        <Draggable handle=".task-form-header">
-            <div className={`task-form-container ${darkMode ? 'dark-task-container' : ''}`}>
-                <div className={`task-form ${darkMode ? 'dark-task-form' : ''}`}>
-                    <div className={`task-form-header ${darkMode ? 'dark-task-form-header' : ''}`}>
-                        <button className="close-button" onClick={closeForm}>
-                            X
-                        </button>
-                    </div>
-                    <form onSubmit={handleSubmit}>
-                        <input
-                            type="text"
-                            id="title"
-                            value={title}
-                            onChange={(e) => setTitle(e.target.value)}
-                            placeholder="Title"
-                            required
-                        />
-                        <label htmlFor="dueTime">Due Date</label>
-                        <input
-                            type="datetime-local"
-                            id="dueTime"
-                            value={dueTime}
-                            onChange={(e) => setDueTime(e.target.value)}
-                            required
-                        />
-                        <textarea
-                            id="description"
-                            value={description}
-                            placeholder="Description"
-                            onChange={(e) => setDescription(e.target.value)}
-                        />
-                        <button type="submit">{isEditing ? "Save" : "Create Task"}</button>
-                    </form>
-                </div>
+    try {
+      const apiUrl = `https://quantumix.onrender.com/api/tasks${isEditing ? `/${task._id}` : ''}`;
+      const method = isEditing ? 'PATCH' : 'POST';
+
+      const response = await fetch(apiUrl, {
+        method: method,
+        headers: {
+          'Authorization': `Bearer ${user.token}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newTask)
+      });
+
+      const responseData = await response.json();
+
+      if (response.ok) {
+        isEditing ? onUpdateTask(responseData) : onAddTask(responseData);
+        closeForm();
+      } else {
+        console.error('Failed to create or update task:', responseData);
+        alert('Failed to create or update task. Please try again.');
+      }
+    } catch (err) {
+      console.error('An error occurred:', err);
+      alert('An error occurred. Please try again.');
+    }
+  };
+
+  return (
+    <div className={`task-form-container ${darkMode ? 'dark-task-container' : ''}`}>
+      <div className={`task-form ${darkMode ? 'dark-task-form' : ''}`}>
+        <div className={`task-form-header ${darkMode ? 'dark-task-form-header' : ''}`}>
+          <button className="close-button" onClick={closeForm}>X</button>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Title"
+            required
+          />
+          <label htmlFor="dueTime">Due Date</label>
+          <input
+            type="datetime-local"
+            id="dueTime"
+            value={dueTime}
+            onChange={(e) => setDueTime(e.target.value)}
+            required
+          />
+          <textarea
+            id="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            placeholder="Description"
+          />
+          <div className="tags-section">
+            {Array.isArray(tags) && tags.map((tag, index) => (
+              <div key={index} className="tag">
+                <input 
+                  type="text" 
+                  name="name" 
+                  value={tag.name} 
+                  onChange={(e) => handleTagChange(index, e)} 
+                  placeholder="Tag Name" 
+                />
+                <input 
+                  type="color" 
+                  name="color" 
+                  value={tag.color} 
+                  onChange={(e) => handleTagChange(index, e)} 
+                />
+                <button type="button" onClick={() => removeTag(index)}>Remove</button>
+              </div>
+            ))}
+            <button type="button" onClick={addTag}>Add Tag</button>
+          </div>
+          <div className="form-buttons">
+            <button type="submit" className={`button ${isEditing ? "rounded-left" : "rounded-full"}`}>
+                {isEditing ? "Update Task" : "Create Task"}
+            </button>
+            {isEditing && (
+                <button type="button" className="delete-button rounded-right" onClick={() => setShowDeleteModal(true)}>
+                Delete Task
+                </button>
+            )}
             </div>
-        </Draggable>
-    );
+        </form>
+      </div>
+  
+      {showDeleteModal && (
+        <div className="delete-modal">
+          <div className="delete-modal-content">
+            <h2>Are you sure you want to delete this task?</h2>
+            <div className="delete-modal-buttons">
+              <button onClick={handleDelete}>Yes</button>
+              <button onClick={() => setShowDeleteModal(false)}>No</button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default TaskForm;
