d05b95b2e768572916584a18caaf4234ef484c55
Devin W Bowler <83326036+devinbowler@users.noreply.github.com>
2023-12-01 22:11:25 -0500

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import "./Home.css";
+import { useAuthContext } from "../hooks/useAuthContext";
+
 
 function Home() {
-  return <h2>Welcome to the Home page!</h2>;
+  const [userName, setUserName] = useState('User');
+  const [events, setEvents] = useState([]);
+  const { user } = useAuthContext();
+
+  // Fetch Events
+  useEffect(() => {
+    const fetchEvents = async () => {
+      if (user) {
+        const response = await fetch('https://quantumix.onrender.com/api/schedule', {
+          headers: {
+            'Authorization': `Bearer ${user.token}`
+          }
+        });
+        const json = await response.json();
+        if (response.ok) {
+          setEvents(json);
+        }
+      }
+    };
+    fetchEvents();
+  }, [user]);
+
+  // Function to format date and time
+  const formatDateTime = (dateTime) => {
+    // Format the date and time from the dateTime string
+    const date = new Date(dateTime);
+    return date.toLocaleString(); // Adjust the format as needed
+  };
+
+  return (
+    <div className="dashboard">
+      <h1>Hello, {userName}</h1>
+      <div className="events-container">
+        {events.map(event => (
+          <div key={event._id} className="event">
+            <h3>{event.title}</h3>
+            <p>{formatDateTime(event.startT)} - {formatDateTime(event.endT)}</p>
+            <p>{event.desc}</p>
+          </div>
+        ))}
+      </div>
+      {/* Add sections for tasks and projects here */}
+    </div>
+  );
 }
 
 export default Home;
