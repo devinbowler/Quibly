0cd852c78dfb6e0d755c816f19c638544545ff31
Devin W Bowler <83326036+devinbowler@users.noreply.github.com>
2024-02-20 00:07:09 -0500

    Add files via upload

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -1,89 +1,152 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback, useRef  } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { useAuthContext } from '../hooks/useAuthContext';
 import { useTheme } from '../ThemeContext';
+import { debounce } from 'lodash';
+import MonacoEditor from 'react-monaco-editor';
 import './Project.css';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/default.css'; 
 
 function Project() {
   const location = useLocation();
-  const project = location.state.project;
+  const project = location.state?.project || {};
   const navigate = useNavigate();
   const { user } = useAuthContext();
   const { darkMode } = useTheme();
-
-  const [title, setTitle] = useState(project.title);
+  const [title, setTitle] = useState(project.title || '');
+  // Initialize content with a single text block containing the backend text
+  const [content, setContent] = useState([{ type: 'text', value: project.text || '' }]);
   const [text, setText] = useState(project.text || '');
+  const [saveStatus, setSaveStatus] = useState("All changes saved");
 
   useEffect(() => {
-    // Apply overflow-y hidden to the body when the component mounts
-    document.body.style.overflowY = 'hidden';
+    // Apply syntax highlighting for code blocks
+    document.querySelectorAll('pre code').forEach((block) => {
+        hljs.highlightBlock(block);
+    });
+}, [content]);
 
-    // Revert back to the default overflow-y style when the component unmounts
-    return () => {
-      document.body.style.overflowY = 'auto';
-    };
-  }, []);
+const handleTextChange = (e) => {
+  setText(e.target.value);
+};
 
-  const handleTitleChange = (event) => {
-    setTitle(event.target.value);
-  };
+  function handleKeyDown(event, index) {
+    const { key, target } = event;
+    let block = content[index];
+    let newValue = block.value;
+    const { selectionStart, value } = target;
+  
+    // Handling bullet insertion and initial indentation with "* " followed by space
+    if (key === " " && value.substring(selectionStart - 1, selectionStart) === "*") {
+      event.preventDefault();
+      const indent = "  "; // Using two spaces for the initial indent
+      newValue = value.substring(0, selectionStart - 1) + indent + "• " + value.substring(selectionStart);
+      // Update content
+      updateContentValue(index, newValue);
+    }
+    
+    // Handling indentation and bullet style cycling with Tab
+    else if (key === "Tab") {
+      event.preventDefault();
+      const beforeCursor = value.substring(0, selectionStart);
+      const afterCursor = value.substring(selectionStart);
+      const lineStart = beforeCursor.lastIndexOf('\n') + 1;
+      const lineText = beforeCursor.substring(lineStart);
+      const indent = "  "; // Define the increment of indentation
+      
+      let newLineText = lineText;
+      let indentChange = 0; // Track change in indentation for cursor adjustment
+      
+      // Apply changes based on current bullet
+      if (lineText.includes("•")) {
+        newLineText = lineText.replace("•", indent + "◦");
+        indentChange = indent.length; // Increase indentation
+      } else if (lineText.includes("◦")) {
+        newLineText = lineText.replace("◦", indent + "▪");
+        indentChange = indent.length; // Further increase indentation
+      } else if (lineText.includes("▪")) {
+        newLineText = lineText.replace("▪", "•");
+        indentChange = 0; // Reset indentation
+      }
+      
+      newValue = value.substring(0, lineStart) + newLineText + afterCursor;
+      // Update content
+      updateContentValue(index, newValue);
+    }
+  }
+  
+  // Function to update the value of a content block and maintain the rest of the content
+  function updateContentValue(index, newValue) {
+    const updatedContent = content.map((item, idx) => idx === index ? { ...item, value: newValue } : item);
+    setContent(updatedContent);
+  }
 
-  const handleTextChange = (event) => {
-    setText(event.target.value);
-  };
-
-  const handleBack = () => {
-    navigate('/app/visionboard');
-  };
-
-  const handleSave = async () => {
+  const saveChanges = async () => {
+    setSaveStatus("Saving...");
     try {
+      // Construct payload from content
+      const payload = {
+        title,
+        text: content.map(item => item.value).join('\n'), // Combine all content into a single string for saving
+      };
+      // Simulate a save operation with a PATCH request
       const response = await fetch(`https://quantumix.onrender.com/api/projects/${project._id}`, {
         method: 'PATCH',
         headers: {
+          'Content-Type': 'application/json',
           'Authorization': `Bearer ${user.token}`,
-          'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ title, text }) // Update with title and text
+        body: JSON.stringify(payload),
       });
-  
+
       if (response.ok) {
-        console.log("Project updated successfully");
+        setSaveStatus("All changes saved");
       } else {
-        console.error("Failed to update project");
+        setSaveStatus("Error saving changes");
       }
     } catch (error) {
-      console.error("Error updating project:", error);
+      console.error("Failed to save changes:", error);
+      setSaveStatus("Error saving changes");
     }
   };
-  
+
+  const debouncedSaveChanges = useCallback(debounce(saveChanges, 2000), [title, content]);
+
+  useEffect(() => {
+    if (content.length > 0) {
+      debouncedSaveChanges();
+    }
+    // Cleanup function to cancel any pending save when the component unmounts or content changes
+    return () => {
+      debouncedSaveChanges.cancel();
+    };
+  }, [content, debouncedSaveChanges]);
+
 
   return (
-    <div className={`project ${darkMode ? "dark-mode" : ""}`}>
+    <div className={`project ${darkMode ? 'dark-mode' : ''}`}>
       <div className="static-head">
-      <button className="back-button" onClick={handleBack}>
-        <FiArrowLeft />
-      </button>
-      <div className="header">
+        <button className="back-button" onClick={() => navigate('/app/visionboard')}>
+          <FiArrowLeft />
+        </button>
         <input
           type="text"
           value={title}
-          onChange={handleTitleChange}
+          onChange={(e) => setTitle(e.target.value)}
           className="project-title"
           placeholder="Untitled"
         />
-        <button onClick={handleSave} className="save-button">Save</button>
-      </div>
+        <div className="save-status">{saveStatus}</div>
       </div>
       <textarea
         value={text}
         onChange={handleTextChange}
         className="project-text"
-        placeholder="This project will..."
+        placeholder="Start typing here..."
       />
     </div>
   );
 }
-
 export default Project;
