3e1b2e18d92442040747010ab563306f0074ebf3
Devin W Bowler <devinbowler@gmail.com>
2025-02-13 18:25:00 -0500

    I WAS ON QUANTIMIX OLD API THIS WHOLE TIME WTFFFFFFFFFFFF.

--- client/src/hooks/useSignup.js
+++ client/src/hooks/useSignup.js
@@ -11,7 +11,7 @@ export const useSignup = () => {
         setError(null);
 
         try {
-            const response = await fetch('https://quantumix.onrender.com/api/user/signup', {
+            const response = await fetch('https://quibly.onrender.com/api/user/signup', {
                 method: 'POST',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify({email, password})
