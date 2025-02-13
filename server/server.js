1dfdc22abcce678edc59a9a859afce5bdd6003d4
Devin W Bowler <devinbowler@gmail.com>
2025-02-12 20:15:31 -0500

    Cors updates.

--- server/server.js
+++ server/server.js
@@ -19,9 +19,17 @@ const allowedOrigins = [
 
 // CORS configuration
 app.use(cors({
-  origin: '*', // Allow all origins
-  credentials: true, // Allow credentials
-}));
+    origin: (origin, callback) => {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, origin);
+      } else {
+        return callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  }));
 
 // Middleware
 app.use(express.json());
