21d3871d29b62d4975d41fc95dc0f0fc80d627a0
Devin Bowler <83326036+devinbowler@users.noreply.github.com>
2025-03-29 12:57:59 -0400

    Update task.js

--- client/src/pages/task.js
+++ client/src/pages/task.js
@@ -600,7 +600,7 @@ return (
       )}
     </div>
 
-    {currentPath !== "system:/user/" || viewType !== 'note' && (
+    {currentPath !== "system:/user/" || viewType !== 'note' || viewType !== 'task' && (
       <div className="back-button-container">
         <button className="back-button" onClick={goBackInPath}>← Back</button>
       </div>
